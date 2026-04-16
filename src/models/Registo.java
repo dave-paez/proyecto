01bc7246a107e16bca8badcfe57f497f55fe2552
dave-paez <davepaezm@gmail.com>
2026-04-16 13:23:09 -0500

    Añadido clase padre

---
+++ src/models/Registo.java
@@ -0,0 +1,14 @@
+package models;
+
+public class Registo {
+    protected String id;
+    protected String nombre;
+
+    public Registo(String id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public Registo() {
+    }
+}
