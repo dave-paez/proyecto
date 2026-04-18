2854323d6e64fc930e3ad53efb45fa5362da9b3b
Juan Jose Duarte Urrego <cuartoa2016juanjose@gmail.com>
2026-04-18 11:50:29 -0500

    creacion de las clases padres

---
+++ src/models/Gestionable.java
@@ -0,0 +1,9 @@
+package models;
+
+//Primer interface - creado por juan duarte
+public interface Gestionable {
+
+  void cambiarEstado(String nuevoEstado);
+
+  String obtenerDetalles();
+}
---
+++ src/models/Persona.java
@@ -0,0 +1,18 @@
+package models;
+
+//primera clase padre - creado por dave paez
+//modificado por juan duarte
+
+public abstract class Persona {
+  protected String id;
+  protected String nombre;
+  protected String correo;
+
+  public Persona(String id, String nombre, String correo) {
+    this.id = id;
+    this.nombre = nombre;
+    this.correo = correo;
+  }
+
+  public abstract String obtenerRolDelSistema();
+}
